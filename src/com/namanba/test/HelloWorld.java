e54cabc86b09cdd1b50a5cdb3320cc2a9c967c9d
iamshb <iamshb@naver.com>
2016-11-24 02:25:55 +0900

    added HelloWorld.java

---
+++ src/com/namanba/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.namanba.test;
+
+/**
+ * Created by contacted on 2016. 11. 24..
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.print("Real Hello world!");
+    }
+}
