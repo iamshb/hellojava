ae66f4f716b0ac16cf513e1ac171ba8274bdb3e8
hbshin's macbookPro <iamshb@linecorp.com>
2017-01-02 14:25:10 +0900

    algorithm test 01 commit

---
+++ src/com/namanba/algorithm/Case01.java
@@ -0,0 +1,10 @@
+package com.namanba.algorithm;
+
+/**
+ * Created by lineplus on 2017. 1. 2..
+ */
+public class Case01 {
+    public static void main(String[] args) {
+        System.out.print("test");
+    }
+}
